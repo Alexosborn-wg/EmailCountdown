858fa636e1890714522c76819c37cd122aacbdb5
Alexosborn-wg <alex.osborn@belconnen.worldgym.com.au>
2026-06-09 15:00:17 +1000

    Update gif.php

--- gif.php
+++ gif.php
@@ -22,8 +22,8 @@
 		'angle' => 0, // Angle of the text
 		'x-offset' => 7, // The larger the number the further the distance from the left hand side, 0 to align to the left.
 		'y-offset' => 30, // The vertical alignment, trial and error between 20 and 60.
-		'file' => __DIR__ . DIRECTORY_SEPARATOR . 'Futura.ttc', // Font path
-		'color' => imagecolorallocate($image, 55, 160, 130), // RGB Colour of the text
+		'file' => 'Arial', // Fixed font path to standard Arial
+		'color' => imagecolorallocate($image, 255, 255, 255), // RGB Colour changed to White
 	);
 	for($i = 0; $i <= 60; $i++){
 		
